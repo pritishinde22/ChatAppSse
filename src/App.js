21a1200da2751bff456cbf959403cad1c6d150a9
PritiS <priti.shinde@gupshup.io>
2022-10-18 11:54:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import "./App.css";
+import axios from "axios";
 function App() {
+  const [facts, setFacts] = useState([]);
+  const [listening, setListening] = useState(false);
+  const { register, handleSubmit } = useForm();
+  const [name, setName] = useState();
+  const [msg, setMsg] = useState();
+
+  const onSubmit = (e) => {
+    const data = {
+      username: e.username,
+      message: e.chat,
+    };
+
+    axios
+      .post("http://localhost:3000/post", data)
+      .then(() => {
+        setFacts((facts) => facts.concat(data));
+        // setFacts(data);
+        setName(e.username);
+        setMsg(e.chat);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+  // console.log("facts",facts)
+
+  useEffect(() => {
+    if (!listening) {
+      const events = new EventSource("http://localhost:3000/events");
+
+      events.onmessage = (event) => {
+        // console.log("event", JSON.parse(event.data));
+        // const parsedData = JSON.parse(event.data);
+
+        // setFacts((facts) => facts.concat(parsedData));
+        setFacts(JSON.parse(event.data));
+      };
+
+      setListening(true);
+      setMsg("");
+    }
+  }, [listening, facts, msg, name]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+      <section className="chat-section">
+        <div className="msgArea">
+          {facts &&
+            facts?.map((fact, id) => (
+              <div className="incoming" key={id}>
+                <h4>{fact?.username}</h4>
+                <p>{fact?.message}</p>
+              </div>
+            ))}
+        </div>
+      </section>
+      <br /> <br />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          name="username"
+          className="username"
+          type="text"
+          required
+          {...register("username")}
+          placeholder="Enter your name"
+          disabled={name}
+        />
+        <div style={{ display: "flex" }}>
+          <textarea
+            type="text"
+            required
+            name="chat"
+            {...register("chat")}
+            id="textarea"
+            rows="1"
+            value={msg}
+            onChange={(e) => setMsg(e.target.value)}
+            placeholder="Write a message..."
+          />
+          <button type="submit" className="submitButton">
+            Send
+          </button>
+        </div>
+      </form>
+    </>
   );
 }
 
